d4952d4fe9c5ee0d6ae5fcf8dcae913e47c43678
Angel Valladares <arivalladares2.0@gmail.com>
2024-02-07 09:22:01 -0600

    Refactor password reset component (linter)

--- client/src/temp_components/SetNewPassword.jsx
+++ client/src/temp_components/SetNewPassword.jsx
@@ -1,16 +1,12 @@
-import  { useState } from 'react';
-import { IconEye, IconEyeOff } from '@tabler/icons-react'; 
-
+import { useState } from "react";
+import { IconEye, IconEyeOff } from "@tabler/icons-react";
 
 const PasswordResetComponent = () => {
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
-  const [error, setError] = useState(''); 
-
-
-  
+  const [error, setError] = useState("");
 
   const handleNewPasswordChange = (event) => {
     setNewPassword(event.target.value);
@@ -19,8 +15,6 @@ const PasswordResetComponent = () => {
   const handleConfirmPasswordChange = (event) => {
     setConfirmPassword(event.target.value);
   };
-
- 
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
@@ -33,66 +27,99 @@ const PasswordResetComponent = () => {
   const validateAndSubmit = (event) => {
     event.preventDefault();
     if (newPassword !== confirmPassword) {
-      setError('Las contraseñas no coinciden');
+      setError("Las contraseñas no coinciden");
     } else {
-      setError('');
+      setError("");
     }
   };
 
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Resetear Contraseña</h2>
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Resetear Contraseña
+        </h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST"  onSubmit={validateAndSubmit}>
-        {error && <p className="text-red-500">{error}</p>}
+        <form
+          className="space-y-6"
+          action="#"
+          method="POST"
+          onSubmit={validateAndSubmit}
+        >
+          {error && <p className="text-red-500">{error}</p>}
 
           <div>
-            <label htmlFor="newPassword" className="block text-sm font-medium leading-6 text-gray-900">Nueva Contraseña</label>
+            <label
+              htmlFor="newPassword"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Nueva Contraseña
+            </label>
             <div className="relative mt-2">
-      <input
-        id="newPassword"
-        name="newPassword"
-        type={passwordVisible ? "text" : "password"} // Cambia el tipo según el estado de passwordVisible
-        autoComplete="off"
-        required
-        className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
-        value={newPassword}
-        onChange={handleNewPasswordChange}
-      />
-      <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
-        {passwordVisible ? (
-          <IconEyeOff color="gray" size={24} onClick={togglePasswordVisibility} />
-        ) : (
-          <IconEye color="gray" size={24} onClick={togglePasswordVisibility} />
-        )}
-      </div>
-    </div>
+              <input
+                id="newPassword"
+                name="newPassword"
+                type={passwordVisible ? "text" : "password"} // Cambia el tipo según el estado de passwordVisible
+                autoComplete="off"
+                required
+                className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+                value={newPassword}
+                onChange={handleNewPasswordChange}
+              />
+              <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
+                {passwordVisible ? (
+                  <IconEyeOff
+                    color="gray"
+                    size={24}
+                    onClick={togglePasswordVisibility}
+                  />
+                ) : (
+                  <IconEye
+                    color="gray"
+                    size={24}
+                    onClick={togglePasswordVisibility}
+                  />
+                )}
+              </div>
+            </div>
           </div>
 
           <div>
-            <label htmlFor="confirmPassword" className="block text-sm font-medium leading-6 text-gray-900">Confirmar Nueva Contraseña</label>
+            <label
+              htmlFor="confirmPassword"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Confirmar Nueva Contraseña
+            </label>
             <div className="relative mt-2">
-      <input
-        id="confirmPassword"
-        name="confirmPassword"
-        type={confirmPasswordVisible ? "text" : "password"} // Cambia el tipo según el estado de passwordVisible
-        autoComplete="off"
-        required
-        className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
-        value={confirmPassword}
-        onChange={handleConfirmPasswordChange}
-      />
-      <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
-        {confirmPasswordVisible ? (
-          <IconEyeOff color="gray" size={24} onClick={toggleConfirmPasswordVisibility} />
-        ) : (
-          <IconEye color="gray" size={24} onClick={toggleConfirmPasswordVisibility} />
-        )}
-      </div>
-    </div>
+              <input
+                id="confirmPassword"
+                name="confirmPassword"
+                type={confirmPasswordVisible ? "text" : "password"} // Cambia el tipo según el estado de passwordVisible
+                autoComplete="off"
+                required
+                className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+              />
+              <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
+                {confirmPasswordVisible ? (
+                  <IconEyeOff
+                    color="gray"
+                    size={24}
+                    onClick={toggleConfirmPasswordVisibility}
+                  />
+                ) : (
+                  <IconEye
+                    color="gray"
+                    size={24}
+                    onClick={toggleConfirmPasswordVisibility}
+                  />
+                )}
+              </div>
+            </div>
           </div>
 
           <div>
