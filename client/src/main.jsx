7162e2604836cce607cbaec288641c33c77a25a1
Angel Valladares <arivalladares2.0@gmail.com>
2024-02-07 00:55:25 -0600

    Add ChangePassword component to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ChangePassword from './temp_components/ChangePassword.jsx'
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ChangePassword />
   </React.StrictMode>,
 )
