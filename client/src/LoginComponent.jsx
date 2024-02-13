f8ab3faf16d3262afd8a7894fd8081ac5f3a33a0
Angel Valladares <arivalladares2.0@gmail.com>
2024-02-13 04:48:08 -0600

    Update login component text

--- client/src/LoginComponent.jsx
+++ client/src/LoginComponent.jsx
@@ -82,7 +82,9 @@ export default function LoginComponent() {
                     <div className="w-full border-t border-gray-200" />
                   </div>
                   <div className="relative flex justify-center text-sm font-medium leading-6">
-                    <span className="bg-white px-6 text-gray-900">O continua con</span>
+                    <span className="bg-white px-6 text-gray-900"> 
+                        O inicia sesión con
+                    </span>
                   </div>
                 </div>
   
