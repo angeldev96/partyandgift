f0b749d3be45d78b00a18fc50caec6ae4d8a162f
Angel Valladares <arivalladares2.0@gmail.com>
2024-02-13 04:40:50 -0600

    Update page title and add login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
-
+import LoginComponent from "./LoginComponent";
 
 
 function App() {
   return (
-    <h1 className="text-3xl text-red-600 font-bold underline">
-      Hello world!
-    </h1>
+      <LoginComponent />
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 // import ChangePassword from './temp_components/ChangePassword.jsx'
-import PasswordReset from './temp_components/PasswordReset.jsx'
+// import PasswordReset from './temp_components/PasswordReset.jsx'
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App />
-    <ChangePassword /> */}
-    <PasswordReset />
+    <App />
+    {/* <ChangePassword /> */}
+    {/* <PasswordReset /> */}
   </React.StrictMode>,
 )
