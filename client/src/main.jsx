60b5f5a33e78d86eada19692af1de0bf66db9ea3
Angel Valladares <arivalladares2.0@gmail.com>
2024-02-18 12:42:27 -0600

    Update favicon

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,8 +8,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <ChangePassword />
+    <App />
+    {/* <ChangePassword /> */}
     {/* <PasswordReset /> */}
   </React.StrictMode>,
 )
