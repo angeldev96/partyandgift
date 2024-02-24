5176f2bbaebac782321414a94e6ed449b2241857
Angel Valladares <arivalladares2.0@gmail.com>
2024-02-23 22:16:55 -0600

    Add jwt for user authentication

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 // Importar las dependencias necesarias
 const express = require('express');
-const session = require('express-session');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const db = require('./utils/database'); 
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
 
 // Crear una instancia de Express
 const app = express();
@@ -15,31 +16,24 @@ app.use(bodyParser.json());
 // Middleware para analizar solicitudes codificadas en URL
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Middleware para manejar sesiones de usuario
-app.use(session({
-  secret: 'maiden', // Cambia esta clave por una segura en un entorno de producción
-  resave: false,
-  saveUninitialized: false
-}));
-
 app.use(cors({
   origin: 'http://localhost:5173'
 }));
 
-// Ruta para el inicio de sesión
+// Route for login
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
-  // Busca el usuario en la base de datos
+  // Search for the user in the database
   const user = await db.getUserByEmail(email);
   if (!user) {
-    return res.status(401).send('Usuario no encontrado');
+    return res.status(401).send('User not found');
   }
-  // Comprueba la contraseña
+  // Check the password
   if (bcrypt.compareSync(password, user.password)) {
-    req.session.userId = user.id;
-    return res.send('Inicio de sesión exitoso');
+    let token = jwt.sign({id: user.id}, 'secretkey', { expiresIn: '1h' });
+    return res.json({message: 'Successful login', token: token});
   } else {
-    return res.status(401).send('Correo electrónico o contraseña incorrectos');
+    return res.status(401).send('Incorrect email or password');
   }
 });
 
@@ -58,16 +52,7 @@ app.post('/register', async (req, res) => {
   return res.send('Usuario registrado exitosamente');
 });
 
-// Ruta para cerrar sesión
-app.post('/logout', (req, res) => {
-  req.session.destroy();
-  res.send('Sesión cerrada exitosamente');
-});
 
-// Iniciar el servidor en el puerto 3001
-app.listen(3001, () => {
-  console.log('Servidor iniciado en el puerto 3001');
-});
 
 // Ruta de registro empleado
 app.post('/register/empleado', async (req, res) => {
@@ -100,4 +85,10 @@ app.post('/login/empleado', async (req, res) => {
   } else {
     return res.status(401).send('Correo electrónico o contraseña incorrectos');
   }
+});
+
+
+// Iniciar el servidor en el puerto 3001
+app.listen(3001, () => {
+  console.log('Servidor iniciado en el puerto 3001');
 });
