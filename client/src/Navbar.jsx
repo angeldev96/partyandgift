cb59a132730b0476b8ef2dbdad34c97f51466eab
Angel Valladares <arivalladares2.0@gmail.com>
2024-02-24 22:02:59 -0600

    Update Navbar component styling and links

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -3,22 +3,22 @@ import PropTypes from 'prop-types';
 
 function Navbar({ isLoggedIn, handleLogout }) {
   return (
-    <nav className="flex justify-end p-4 bg-gray-200">
-      {isLoggedIn ? (
+    <nav className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
+    {isLoggedIn ? (
         <>
-          <Link to="/dashboard" className="mx-2">
-            Dashboard
+          <Link to="/dashboard" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">
+            Store
           </Link>
-          <button onClick={handleLogout} className="mx-2">
+          <button onClick={handleLogout} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Cerrar Sesión
           </button>
         </>
       ) : (
         <>
-          <Link to="/login" className="mx-2">
+          <Link to="/login" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">
             Iniciar Sesión
           </Link>
-          <Link to="/signup" className="mx-2">
+          <Link to="/signup" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Registrarse
           </Link>
         </>
