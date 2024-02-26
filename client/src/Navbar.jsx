be8bf1fc96157cd1e396fa0e0547dfe4327cfe3b
Angel Valladares <arivalladares2.0@gmail.com>
2024-02-25 21:27:50 -0600

    Update Navbar image and adjust alignment

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -6,17 +6,18 @@ function Navbar({ isLoggedIn, handleLogout }) {
     <nav className="mx-auto flex justify-between max-w-7xl items-center gap-x-6 p-6 lg:px-8" aria-label="Global">
     <Link to="/dashboard">
         <span className="sr-only">Dashboard</span>
-        <img className="h-16 w-auto" src="./partyandgift.jpeg" alt="" />
+        <img className="h-16 w-auto" src="./partyandgift.png" alt="" />
     </Link>
     {isLoggedIn ? (
-        <div className="flex gap-x-6">
-            <button onClick={handleLogout} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Cerrar Sesión
-            </button>
-            <Link to="/account" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">
-                Mi Perfil
-            </Link>
-        </div>
+        <div className="flex gap-x-6 items-center">
+        <button onClick={handleLogout} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          Cerrar Sesión
+        </button>
+        <Link to="/account" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 ">
+          Mi Perfil
+        </Link>
+      </div>
+      
     ) : (
         <div className="flex gap-x-6">
             <Link to="/login" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">
