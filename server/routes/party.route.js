2b3b7d9b8a0a1d7977e4cbd8532a471da6a87c10
Angel Valladares <arivalladares2.0@gmail.com>
2024-03-09 02:36:33 -0600

    Delete unnecessary files and code

--- server/routes/party.route.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express')
-const {getLogin} = require('../controllers/party.controllers')
-
-const router = Router()
-
-router.get('/login', getLogin)
-
-export default getLogin;
