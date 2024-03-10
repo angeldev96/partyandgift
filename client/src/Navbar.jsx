b22683c9d9fa3a55678c68dc9d5be18762d36109
carloquer <benitezcarlosnote4@gmail.com>
2024-03-10 16:21:29 -0600

    mejoras en la navbar

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { HiOutlineShoppingCart } from 'react-icons/hi';
+import { HiUserCircle } from 'react-icons/hi';
+import { FaSignOutAlt } from 'react-icons/fa';
 
 function Navbar({ isLoggedIn, handleLogout }) {
   return (
@@ -11,16 +13,19 @@ function Navbar({ isLoggedIn, handleLogout }) {
     </Link>
     {isLoggedIn ? (
         <div className="flex gap-x-6 items-center">
-        <button onClick={handleLogout} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          Cerrar Sesión
-        </button>
-        <Link to="/account" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 ">
-          Mi Perfil
-        </Link>
-        <Link to="/order_address" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">
+        <Link to="/order_address" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 hover:text-blue-400">
         <HiOutlineShoppingCart className="inline-block mr-1 h-5 w-5" /> Carrito
         </Link>
-      </div>
+        <Link to="/account" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 hover:text-blue-400">
+        <HiUserCircle className="inline-block mr-1 h-5 w-5" /> Mi Perfil
+        </Link>
+        <button 
+         onClick={handleLogout} 
+        className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-800 shadow-sm hover:bg-red-600 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 flex items-center space-x-2">  <FaSignOutAlt />
+        Cerrar Sesión
+        </button>
+
+       </div>
       
     ) : (
         <div className="flex gap-x-6 items-center">
