691df85e6f56a7edf9ed4e0be2083a51087532d0
carloquer <benitezcarlosnote4@gmail.com>
2024-03-10 20:37:54 -0600

    actualizacion navbar

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,49 +1,84 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { HiOutlineShoppingCart } from 'react-icons/hi';
-import { HiUserCircle } from 'react-icons/hi';
+import { HiOutlineDocumentText, HiOutlineInformationCircle } from 'react-icons/hi';
+import { HiOutlineShoppingCart, HiUserCircle } from 'react-icons/hi';
 import { FaSignOutAlt } from 'react-icons/fa';
 
 function Navbar({ isLoggedIn, handleLogout }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="mx-auto flex justify-between max-w-7xl items-center gap-x-6 lg:px-8" aria-label="Global">
-    <Link to="/dashboard">
-        <span className="sr-only">Dashboard</span>
+      <Link to="/dashboard" className="flex items-center gap-x-4">
         <img className="h-16 w-auto" src="./partyandgift.png" alt="" />
-    </Link>
-    {isLoggedIn ? (
-        <div className="flex gap-x-6 items-center">
-        <Link to="/order_address" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 hover:text-blue-400">
-        <HiOutlineShoppingCart className="inline-block mr-1 h-5 w-5" /> Carrito
-        </Link>
-        <Link to="/account" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 hover:text-blue-400">
-        <HiUserCircle className="inline-block mr-1 h-5 w-5" /> Mi Perfil
-        </Link>
-        <button 
-         onClick={handleLogout} 
-        className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-800 shadow-sm hover:bg-red-600 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 flex items-center space-x-2">  <FaSignOutAlt />
-        Cerrar Sesión
-        </button>
-
-       </div>
-      
-    ) : (
-        <div className="flex gap-x-6 items-center">
-            <Link to="/login" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">
-                Iniciar Sesión
-            </Link>
-            <Link to="/signup" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Registrarse
-            </Link>
+        <div className="flex items-center gap-x-1 text-gray-900">
+          <HiOutlineDocumentText className="h-6 w-6" />
+          <span className="text-lg font-semibold">Inicio</span>
         </div>
-    )}
-</nav>
+        <div className="flex items-center gap-x-1 text-gray-900">
+          <HiOutlineInformationCircle className="h-7 w-7" />
+          <span className="text-lg font-semibold">Acerca</span>
+        </div>
+      </Link>
+      {isLoggedIn ? (
+        <div className="flex gap-x-6 items-center">
+          <div className="relative inline-block text-left">
+            <div>
+              <button
+                type="button"
+                className="inline-flex items-center justify-center w-full px-4 py-2 text-sm font-semibold text-gray-900 bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                onClick={toggleMenu}
+              >
+                <HiOutlineDocumentText className="inline-block mr-1 h-7 w-7" /> Catálogo
+              </button>
+            </div>
+            {isOpen && (
+              <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-10">
+                <Link to="/dulces" className="block px-4 py-2 text-sm text-gray-900 hover:bg-blue-100">Dulces</Link>
+                <Link to="/globos" className="block px-4 py-2 text-sm text-gray-900 hover:bg-blue-100">Globos</Link>
+                <Link to="/arreglos" className="block px-4 py-2 text-sm text-gray-900 hover:bg-blue-100">Arreglos</Link>
+              </div>
+            )}
+          </div>
+          <Link to="/order_address" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 hover:text-blue-400">
+            <HiOutlineShoppingCart className="inline-block mr-1 h-7 w-7" /> Carrito
+          </Link>
+          <Link to="/account" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 hover:text-blue-400">
+            <HiUserCircle className="inline-block mr-1 h-7 w-7" /> Mi Perfil
+          </Link>
+          <button
+            onClick={handleLogout}
+            className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-800 shadow-sm hover:bg-red-600 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 flex items-center space-x-2">
+            <FaSignOutAlt />
+            Cerrar Sesión
+          </button>
+        </div>
+
+      ) : (
+        <div className="flex gap-x-6 items-center">
+          <Link to="/login" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">
+            Catálogo
+          </Link>
+          <Link to="/login" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">
+            Iniciar Sesión
+          </Link>
+          <Link to="/signup" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Registrarse
+          </Link>
+        </div>
+      )}
+    </nav>
   );
 }
 
 Navbar.propTypes = {
-    isLoggedIn: PropTypes.bool.isRequired,
-    handleLogout: PropTypes.func.isRequired,
-  };
+  isLoggedIn: PropTypes.bool.isRequired,
+  handleLogout: PropTypes.func.isRequired,
+};
 
 export default Navbar;
