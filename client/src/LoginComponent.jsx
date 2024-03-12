ab98b5667daf4a1ec7b1e3bc982d650fbe74aceb
EAMR22 <elvis1042.rodriguez@gmail.com>
2024-03-12 08:58:21 -0600

    mejorando el formulario de registro

--- client/src/LoginComponent.jsx
+++ client/src/LoginComponent.jsx
@@ -51,15 +51,14 @@ export default function LoginComponent({setIsLoggedIn}) {
           <ToastContainer />
 
       <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="flex justify-center items-center">
+          <img className="h-16 w-auto" src="./partyandgift.png" alt="" />
+        </div>
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 mb-6 text-center text-2xl font-bold leading-9 tracking-tight text-green-400">
             Inicia sesión
           </h2>
-          <div className="flex justify-center items-center">
-            <img className="h-16 w-auto" src="./partyandgift.png" alt="" />
-          </div>
         </div>
-
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px] border-solid border-2 border-indigo-600 rounded-lg">
           <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
             <form onSubmit={handleSubmit} className="space-y-6">
