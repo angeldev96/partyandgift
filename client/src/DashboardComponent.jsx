6d6fccbce863a223159c7172f6db705a5363168b
Angel Valladares <arivalladares2.0@gmail.com>
2024-04-12 23:26:16 -0600

    Update DashboardComponent.jsx to remove unused import

--- client/src/DashboardComponent.jsx
+++ client/src/DashboardComponent.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
